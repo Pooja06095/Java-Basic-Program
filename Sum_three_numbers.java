70be85988b37449e68b8cfc05393be458618c9c4
Pooja Goswami <apoojagoswami5@gmail.com>
2025-09-15 18:06:52 +0530

    Day02 : Sum_Three_Numbers
    This program  add n numbers in Java.

---
+++ Sum_three_numbers.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Sum_three_numbers {
+    public static void main(Strings[]args) {
+        System.out.println("Sum of these numbers: ");
+        int num1 = 20;
+        int num2 = 20;
+        int num3 = 10;
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
