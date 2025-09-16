f137234aaa65cb3f04d1323ff7e14041161d26dc
Pooja Goswami <apoojagoswami5@gmail.com>
2025-09-16 22:10:46 +0530

    Taking Input from user
    This Program used to take the input from keyboard.

---
+++ Taking_Input_from_user.java
@@ -0,0 +1,15 @@
+package com.company;
+import java.util.Scanner;
+public class Taking_Input_from_user {
+    public static void main(Strings[]args){
+        System.out.println("Taking input from user;");
+    Scanner sc=new Scanner(System.in);
+        System.out.println("Enter first number");
+        int a=sc.nextInt();
+        System.out.println("Enter second number");
+        int b=sc.nextInt();
+        int sum= a+b;
+        System.out.println("Sum of these number is : "+sum);
+
+    }
+}
