b46181f33b141e3aa930c506641f58041b052a5d
Pooja Goswami <apoojagoswami5@gmail.com>
2025-09-17 22:27:47 +0530

    Literals

---
+++ Literals.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Literals {
+    public static void main(Strings[]args){
+        int  i=26;
+        byte b=24;
+        short s=10;
+        long l=28888L;
+        float f=2.3f;
+        double d=2.3d;
+        char c='A';
+        boolean e =true;
+        String str = "pooja";
+        System.out.println(i);
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(e);
+        System.out.println(str);
+
+    }
+}
